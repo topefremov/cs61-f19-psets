d9a04ff89751b96526a3bed5b696c0e7d3c3ea34
Aleksandr Efremov <topefremov@yandex.ru>
2019-09-22 18:14:40 +0300

    wc61 complete

--- pset0/wc61.cc
+++ pset0/wc61.cc
@@ -1,16 +1,36 @@
 #include <cstdio>
+#include <cctype>
 
 int main() {
-    unsigned long count = 0;
-    while (fgetc(stdin) != EOF) {
-        count++;
-    }
+    size_t b_count = 0, w_count = 0, l_count = 0;
+    bool inspace = true;
+    while (true) {
+        int c = fgetc(stdin);
 
+        if (c == EOF) {
+            break;
+        }
+
+        b_count++;
+
+        bool thisspace = isspace(c);
+        if (inspace && !thisspace) {
+            w_count++;
+        }
+
+        inspace = thisspace;
+
+        if (c == '\n') {
+            l_count++;
+        }
+        
+    }
+    
     if (ferror(stdin)) {
         printf("input error\n");
     }
 
-    printf("%8lu\n", count);
+    printf("%8lu %7lu %7lu\n", l_count, w_count, b_count);
     
     return 0;
 }
